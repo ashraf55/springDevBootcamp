a762cbd9341113053572aa8ef55b0532a0ab47a7
Ashraful Karim <Ashraful@Ashrafuls-MBP.netis.cc>
2024-12-28 09:46:55 +0600

    initializing the project

---
+++ src/main/java/com/udemy/springDevBootcamp/SpringDevBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.springDevBootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/springDevBootcamp/SpringDevBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springDevBootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
