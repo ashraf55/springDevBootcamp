ece2a877fdeef322e58c0e2b12b365e20d703f92
ashraful.karim <ashraful.karim@nagad.com.bd>
2025-01-06 12:07:57 +0600

    Section 7 Challenge 1 - DONE

--- src/main/java/com/udemy/springDevBootcamp/SpringDevBootcampApplication.java
+++ src/main/java/com/udemy/springDevBootcamp/SpringDevBootcampApplication.java
@@ -3,11 +3,11 @@ package com.udemy.springDevBootcamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringDevBootcampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDevBootcampApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringDevBootcampApplication.class, args);
+//	}
 
 }
---
+++ src/main/java/com/udemy/springDevBootcamp/painting/PaintingApplication.java
@@ -0,0 +1,12 @@
+package com.udemy.springDevBootcamp.painting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaintingApplication.class, args);
+    }
+
+}
