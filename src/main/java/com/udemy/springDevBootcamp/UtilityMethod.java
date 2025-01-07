7f984a8b3f01d902b734bb7eed5ad0ddc1846980
ashraful.karim <ashraful.karim@nagad.com.bd>
2025-01-07 13:09:24 +0600

    thymeleaf utlitiy workbook practice

---
+++ src/main/java/com/udemy/springDevBootcamp/UtilityMethod.java
@@ -0,0 +1,15 @@
+package com.udemy.springDevBootcamp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UtilityMethod {
+
+    @GetMapping("/name")
+    public String getMethodName(Model model){
+        model.addAttribute("fullName", "Ashraful Karim");
+        return "view";
+    }
+}
