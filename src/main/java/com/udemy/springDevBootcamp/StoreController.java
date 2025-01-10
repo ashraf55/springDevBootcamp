c4936218a5368f57c33229d2ee88761f6debcb2a
Ashraful Karim <karim.mac@gmail.com>
2025-01-10 10:49:49 +0600

    dropdown fixed - page link fixed

--- src/main/java/com/udemy/springDevBootcamp/StoreController.java
+++ src/main/java/com/udemy/springDevBootcamp/StoreController.java
@@ -1,12 +1,20 @@
 package com.udemy.springDevBootcamp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StoreController {
+
     @GetMapping("/")
-    public String getForm(){
+    public String getForm(Model model){
+        model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
+    }
+
+    @GetMapping("/inventory")
+    public String getInventory(){
+        return "inventory";
     }
 }
